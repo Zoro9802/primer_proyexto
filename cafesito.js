62ce45ba01e23102be9cc1558474f53186a7483c
Andrey <carloscabeson@gmail.com>
2022-10-04 19:52:28 -0500

    actualizacion cafetera 04/10

--- cafesito.js
+++ cafesito.js
@@ -1,50 +1,51 @@
-const filefunction = require('./file.fns')
-const readFile = filefunction.readFile
-const saveonFile = filefunction.saveonFile
+const { readFile, saveonFile } = require('./file.fns')
 
 let path = 'cafetera.json'
 
+function agregarAgua(totalAgua, agua) {
+    console.log('hechando Agua')
+    return totalAgua - agua
+}
+function agregarCafe(cafeRestante, cafe) {
+    console.log('hechando cafe')
+    return cafeRestante - cafe
+}
+function agregarAzucar(azucarRestante, azucar) {
+    console.log('hechando azucar')
+    return azucarRestante - azucar
+}
+function agregarLeche(lecheRestante, siUseLeche) {
+    console.log('hechando leche')
+    return lecheRestante - siUseLeche
+}
 function crearCafetera(elAgua, elCafe, elAzucar, laLeche) {
     let cafetera = {
         nivelAgua: elAgua,
         cafeRestante: elCafe,
         azucarRestante: elAzucar,
         lecheRestante: laLeche,
+
         Prepararuncafesito: function (azucar, agua, cafe, conLeche) {
             if (this.nivelAgua < agua) return 'no hay agua'
             if (this.cafeRestante < cafe) return 'no hay cafe'
             if (this.azucarRestante < azucar) return 'no hay azucar'
             if (conLeche && (this.lecheRestante < 1)) return 'no hay leche'
-            function agregarAgua(totalAgua, agua) {
-                console.log('hechando Agua')
-                return totalAgua - agua
-            }
-            function agregarCafe(cafe) {
-                this.cafeRestante = this.cafeRestante - cafe
-                console.log('hechando cafe')
-            }
-            function agregarAzucar(azucar) {
-                this.azucarRestante = this.azucarRestante - azucar
-                console.log('hechando azucar')
-            }
-            function agregarLeche() {
-                this.lecheRestante = this.lecheRestante - 1
-                console.log('hechando leche')
-            }
-            this.nivelAgua = agregarAgua(this.nivelAgua, agua)
-            agregarCafe(cafe)
-            agregarAzucar(azucar)
-            if (conLeche) agregarLeche()
+
             let siUseLeche = 0
             if (conLeche) siUseLeche = 1
+            this.nivelAgua = agregarAgua(this.nivelAgua, agua)
+            this.cafeRestante = agregarCafe(this.cafeRestante, cafe)
+            this.azucarRestante = agregarAzucar(this.azucarRestante, azucar)
+            this.lecheRestante = agregarLeche(this.lecheRestante, siUseLeche)
+
             let restates = {
-                niveldeAgua: cafetera.nivelAgua - agua,
-                cafeRestante: cafetera.cafeRestante - cafe,
-                azucarRestante: cafetera.azucarRestante - azucar,
-                lecheRestante: cafetera.lecheRestante - siUseLeche
+                niveldeAgua: cafetera.nivelAgua,
+                cafeRestante: cafetera.cafeRestante,
+                azucarRestante: cafetera.azucarRestante,
+                lecheRestante: cafetera.lecheRestante
             }
 
-            console.table(this)
+            console.table(restates)
             let indicador1 = 'sin'
             if (conLeche) indicador1 = 'con'
             let indicador2 = 'claro'
@@ -52,7 +53,15 @@ function crearCafetera(elAgua, elCafe, elAzucar, laLeche) {
             if (cafe >= 3) indicador2 = 'oscuro'
             return 'cafesito ' + indicador2 + ' ' + indicador1 + ' leche y ' + azucar + ' porcion de azucar y ' + agua + ' ml de agua'
 
+        },
+        //notificaciones
+        aviso: function (niveldeAgua) {
+            if (niveldeAgua < 200) return 'queda poca agua'
+
+
         }
+
+
     }
     return cafetera
 
@@ -61,9 +70,11 @@ function crearCafetera(elAgua, elCafe, elAzucar, laLeche) {
 let niveles = readFile(path)
 let laCafetera = crearCafetera(niveles.nivelAgua, niveles.cafeRestante, niveles.azucarRestante, niveles.lecheRestante)
 console.table(laCafetera);
+console.log(laCafetera.Prepararuncafesito(1, 250, 1, false))
+console.log(laCafetera.aviso(niveles.nivelAgua))
 saveonFile(path, laCafetera)
 
-console.log(laCafetera.Prepararuncafesito(1, 350, 1, false))
+
 
 // notificacion mediante email de los niveles al estar cerca y al estar empty
 // como rellenar los recibe el item y recibe los valores que deseamos agregar
